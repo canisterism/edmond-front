14f4c3340e9d4776d21559ecada9ac97bbb0c36d
canisterism <ohtthstc@gmail.com>
2019-02-14 00:33:08 +0900

    [add]影とかグラデーション

--- app/components/BookCard.vue
+++ app/components/BookCard.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="column is-one-fifth-desktop is-5-mobile">
-    <div class="box">
+    <div class="card-box-background">
       <figure class="image">
         <img src="https://cover.openbd.jp/9784780802047.jpg" alt>
       </figure>
       <div class="media-content">
         <div class="content">
-          <div class="is-size-5">おにぎりレシピ101 everyday onigiri</div>
-          <div class="is-size-6">山田玲子</div>
+          <div class="is-size-6">おにぎりレシピ101 everyday onigiri</div>
+          <div class="author is-size-7">山田玲子</div>
         </div>
       </div>
     </div>
@@ -19,4 +19,22 @@ export default {}
 </script>
 
 <style lang="scss" scoped>
+.image {
+  margin: 1rem;
+}
+.card-box-background {
+  border-radius: 6px;
+  padding: 1rem;
+  background: linear-gradient(
+    rgba(39, 60, 108, 0.8),
+    rgba(139, 175, 255, 0.3),
+    rgba(39, 60, 108, 0.8)
+  );
+  box-shadow: 10px 5px 5px rgba(10, 10, 10, 0.1),
+    0 0 0 1px rgba(10, 10, 10, 0.1);
+}
+
+.author {
+  margin-top: 0.5rem;
+}
 </style>
